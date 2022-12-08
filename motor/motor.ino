f8312aab2a2c80892e7a817a5021a7511f52c4f2
iago-patrick <iagopatrick14@gmail.com>
2022-12-08 14:36:14 -0300

    adicionando curvas no motor

--- motor/motor.ino
+++ motor/motor.ino
@@ -4,6 +4,7 @@ struct motorA
 //in1 e in2 e o motor A
 int in1;//porta digita do arduino no in1.
 int in2;//porta digita do arduino no in2.
+int enA;//porta digital do arduino no en.
 };
 
 struct motorB
@@ -11,6 +12,7 @@ struct motorB
 //in3 e in4 e o motor B
 int in3;//porta digita do arduino no in3.
 int in4;//porta digita do arduino no in4.
+int enB;//porta digital do arduino na porta en.
 };
 
 motorA mA;
@@ -23,6 +25,8 @@ void setup()
   pinMode(mA.in2, OUTPUT);
   pinMode(mB.in3, OUTPUT);
   pinMode(mB.in4, OUTPUT);
+  pinMode(mA.enA, OUTPUT);
+  pinMode(mB.enB, OUTPUT);
 }
 
 void loop() 
@@ -64,4 +68,24 @@ void motoTras()
   
   digitalWrite(mB.in3, HIGH);
   digitalWrite(mB.in4, LOW);
+}
+void motoCurvaEsquerda()
+{
+  digitalWrite(mA.in1, LOW); 
+  digitalWrite(mA.in2, HIGH);
+  
+  digitalWrite(mB.in3, LOW);
+  digitalWrite(mB.in4, HIGH);
+
+  digitalWrite();
+}
+void motoCurvaDireita()
+{
+  digitalWrite(mA.in1, LOW); 
+  digitalWrite(mA.in2, HIGH);
+  
+  digitalWrite(mB.in3, LOW);
+  digitalWrite(mB.in4, HIGH);
+
+  digitalWrite();
 }
